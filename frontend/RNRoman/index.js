30e5d5e3951e3735f42e845ade6983edf23bccdd
Ricardo Ribeiro Lopes <ricardo.sao@hotmail.com>
2019-05-09 11:08:25 -0300

    Resolendo erros da pasta

--- frontend/RNRoman/index.js
+++
@@ -1,8 +0,0 @@
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
-// import Login from "./src/pages/Login";
-import Projetos from "./src/pages/Projetos";
-// import CadastrarProjeto from "./src/pages/CadastrarProjeto";
-
-AppRegistry.registerComponent(appName, () => Projetos);
